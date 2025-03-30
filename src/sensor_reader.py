704a2cb2d72889b0f68acba8c52d397298a98469
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 19:22:27 +0500

    .

--- src/sensor_reader.py
+++ src/sensor_reader.py
@@ -3,8 +3,9 @@ import Adafruit_DHT
 class DHT11Reader:
     def __init__(self, sensor=Adafruit_DHT.DHT11, pin=7):
         self.sensor = sensor
-        self.pin = pin  # Физический порт 7; при необходимости замените на корректное значение BCM
+        self.pin = pin  # Физический порт 7. При необходимости измените на BCM-номер
 
     def read(self):
-        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
+        # Принудительно указываем платформу для корректной работы
+        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin, platform="raspberrypi")
         return temperature, humidity
